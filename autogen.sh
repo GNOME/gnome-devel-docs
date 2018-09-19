e5fd860cc768ace9d0e1cedf44981765a5803202
David King <amigadave@amigadave.com>
2018-09-19 09:36:54 +0100

    Avoid using gnome-common in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-PKG_NAME="gnome-devel-docs"
-
-(test -f $srcdir/configure.ac \
-  && test -d $srcdir/platform-overview) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
-}
-
-REQUIRED_AUTOMAKE_VERSION=1.6
-export REQUIRED_AUTOMAKE_VERSION
-
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+( cd "$srcdir" && autoreconf --force --install --verbose --warnings=all "$srcdir" )
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
